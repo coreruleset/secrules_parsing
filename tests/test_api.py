a818e8b54fb882011c7fd1603717da9cb9ba28b8
TW - Vincent <315173+touchweb-vincent@users.noreply.github.com>
2025-10-31 11:42:05 +0100

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -88,6 +88,35 @@ def test_collection_argument_with_dollar() -> None:
     assert matched
 
 
+def test_lowercase_and_uppercase_in_argument() -> None:
+    """ Example test showing how to find if a rule has a lowercase transformation, then see if the target
+    of the rule has an uppercase regex. """
+    rule_text = """
+    SecRule REQUEST_FILENAME "@rx /[ABCD]+/$" \
+    "id:1234,\
+    phase:1,\
+    pass,\
+    t:lowercase,\
+    nolog
+    """
+
+    matched = False
+    uppercase_regex = re.compile(r"[A-Z]")
+    parsed_rule = parser.process_from_str(rule_text)
+    for rule in parsed_rule.rules:
+        assert rule.__class__.__name__ == "SecRule"
+        for action in rule.actions:
+            if action.transformations:
+                for t in action.transformations:
+                    if t == "lowercase":
+                        if uppercase_regex.search(rule.operator.rx):
+                            matched = True
+                            assert True, ("Regex tries to match uppercase, "
+                                          "but you are transforming into lowercase so it will "
+                                          "never match")
+
+    assert matched
+
 def test_setvar_declarations() -> None:
     """ Example test various setvar view in the wild """
     rule_text = """
@@ -106,33 +135,6 @@ def test_setvar_declarations() -> None:
     for rule in parsed_rule.rules:
         assert rule.__class__.__name__ == "SecRule"
         // TODO
-
-    assert matched
-
-def test_collection_argument_with_dollar() -> None:
-    """Test that a collection argument can contain `$` (e.g., a key in a JSON document)"""
-    rule_text = """
-    SecRule REQUEST_FILENAME "@rx /apps/mail/api/messages/[0-9]+/flags$" \
-    "id:9508978,\
-    phase:1,\
-    pass,\
-    t:none,\
-    nolog,\
-    ver:'nextcloud-rule-exclusions-plugin/1.0.0',\
-    ctl:ruleRemoveTargetById=942290;ARGS_NAMES:json.flags.$notjunk,\
-    setvar:'tx.allowed_methods=%{tx.allowed_methods} PUT'"
-    """
-
-    parsed_rule = parser.process_from_str(rule_text)
-    matched = False
-    for rule in parsed_rule.rules:
-        assert rule.__class__.__name__ == "SecRule"
-        for action in rule.actions:
-            if action.ctl:
-                matched = True
-                assert action.ctl.ruleRemoveTargetById == 942290
-                assert action.ctl.removeVariable.collection == "ARGS_NAMES"
-                assert action.ctl.removeVariable.collectionArg == "json.flags.$notjunk"
 
     assert matched
 
